b3858e01525a43d19b862dfd85e77dac2844b17c
mikeonslow <mikeonslow@gmail.com>
2019-03-15 03:22:18 -0400

    Temporarily removed email list signup

--- src/DTWConnect.js
+++ src/DTWConnect.js
@@ -1,29 +1,32 @@
 import React, {Component} from 'react';
-import {MDBBtn, MDBContainer, MDBRow, MDBCol, MDBInput, MDBFormInline, MDBIcon} from "mdbreact";
+import {
+  MDBContainer, MDBRow, MDBCol,
+  // MDBBtn, MDBInput, MDBFormInline, MDBIcon
+} from "mdbreact";
 
 class DTWConnect extends Component {
   render() {
     return (
         <MDBContainer className="p-5">
           <MDBRow>
-            <MDBCol>
-              <h1>Stay in Touch</h1>
-              <p className="text-center">Join our mailing list for the latest conference updates. Your email address
-                will be kept private and we'll only update you with with messages relevant to the event.</p>
+            <MDBCol size="12">
+              <h1>Stay up to Date!</h1>
+              {/*<p className="text-center">Join our mailing list for the latest conference updates. Your email address*/}
+                {/*will be kept private and we'll only update you with with messages relevant to the event.</p>*/}
             </MDBCol>
-            <MDBCol size="12" className="text-center">
-              <MDBFormInline className="md-form mr-auto mb-4 justify-content-center">
-                <MDBInput label="Email Address" hint="youremail@domain.com" type="email" size="lg" className="signup"/>
-                <MDBBtn color="secondary" size="md">sign up <MDBIcon far icon="comments"/></MDBBtn>
-              </MDBFormInline>
-            </MDBCol>
-            <MDBCol>
-              <p className="text-center">Or, follow us on
+            {/*<MDBCol size="12" className="text-center">*/}
+            {/*<MDBFormInline className="md-form mr-auto mb-4 justify-content-center">*/}
+            {/*<MDBInput label="Email Address" hint="youremail@domain.com" type="email" size="lg" className="signup"/>*/}
+            {/*<MDBBtn color="secondary" size="md">sign up <MDBIcon far icon="comments"/></MDBBtn>*/}
+            {/*</MDBFormInline>*/}
+            {/*</MDBCol>*/}
+            <MDBCol size="12">
+              <p className="text-center">Follow us on
                 <a href="https://twitter.com/dettechwatch" target="_blank" rel="noopener noreferrer">Twitter</a>, <a
                     href="https://www.facebook.com/DetroitTechWatch/" target="_blank"
                     rel="noopener noreferrer">Facebook</a> or <a
                     href="https://www.instagram.com/detroittechwatch/" target="_blank"
-                    rel="noopener noreferrer">Instagram</a>
+                    rel="noopener noreferrer">Instagram</a> to stay up to date with event information
               </p>
               <a href="dtw_speakers" id="speakers"> </a>
             </MDBCol>
