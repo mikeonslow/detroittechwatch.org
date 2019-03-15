bc5dba00a55f736617f073662dacc183ffc528ad
mikeonslow <mikeonslow@gmail.com>
2019-03-15 01:50:10 -0400

    Completed sponsor section

---
+++ src/DTWSponsorPanel.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardHeader, MDBBtn} from "mdbreact";
+
+class DTWSponsorPanel extends Component {
+  render() {
+    const {color, level, cost, perks} = this.props;
+
+    const listPerks = perks.map((perk) =>
+        <span><hr/>
+          {perk}</span>
+    );
+
+    return (
+        <MDBCard>
+          <MDBCardHeader color={color}>{level} Sponsor</MDBCardHeader>
+          <MDBCardBody>
+            <MDBCardTitle>{cost}</MDBCardTitle>
+            <MDBCardText>
+              <h6>What you Get</h6>
+              {listPerks}
+            </MDBCardText>
+            <a href={`mailto:info@detroittechwatch.org?subject=${level} level sponsor request`}><MDBBtn>Become
+              a {level} Sponsor</MDBBtn></a>
+          </MDBCardBody>
+        </MDBCard>
+    )
+  }
+}
+
+export default DTWSponsorPanel;
--- src/DTWSponsors.js
+++ src/DTWSponsors.js
@@ -1,8 +1,45 @@
 import React, {Component} from 'react';
 import {MDBContainer, MDBRow, MDBCol} from "mdbreact";
+import DTWSponsorPanel from "./DTWSponsorPanel";
+
+const packages = [
+  {
+    "color": "green darken-2",
+    "level": "Lanyard",
+    "cost": "Cost of lanyards & badges",
+    "perks": [
+      "Your company's logo and name printed on each lanyard and badge"
+    ]
+  },
+  {
+    "color": "yellow darken-3",
+    "level": "Gold",
+    "cost": "$1,000",
+    "perks": [
+      "Mention on website",
+      "Mention on all e-mails sent from the conference"
+    ]
+  },
+  {
+    "color": "grey",
+    "level": "Platinum",
+    "cost": "$2,000",
+    "perks": [
+      "Booth at Event",
+      "Mention on website",
+      "Mention on all e-mails sent from the conference"
+    ]
+  },
+];
 
 class DTWSponsors extends Component {
   render() {
+    const listItems = packages.map((item) =>
+        <MDBCol className={`d-flex align-items-stretch`}>
+          <DTWSponsorPanel color={item.color} level={item.level} cost={item.cost} perks={item.perks}/>
+        </MDBCol>
+    );
+
     return (
         <MDBContainer className="p-5">
           <MDBRow>
@@ -11,27 +48,19 @@ class DTWSponsors extends Component {
             </MDBCol>
           </MDBRow>
           <MDBRow>
-            <a href="dtw_code_of_conduct" id="code_of_conduct">&nbsp;</a>
             <MDBCol>
               <h3>Support the community</h3>
-              You or your company can become a sponsor for Detroit Tech Watch 2019.
-
-              For more info, check out becoming a sponsor.
+              <p>You or your company can become a sponsor for Detroit Tech Watch 2019.</p>
             </MDBCol>
           </MDBRow>
+          <MDBRow>
+            {listItems}
+          </MDBRow>
+          <a href="location" id="location">&nbsp;</a>
         </MDBContainer>
     );
   }
 }
-
-
-
-
-
-
-
-
-
 
 
 export default DTWSponsors;
