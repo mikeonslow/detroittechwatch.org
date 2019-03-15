bce3517f0763c80d49ee6ae124c6969a5757fe6e
mikeonslow <mikeonslow@gmail.com>
2019-03-15 01:50:36 -0400

    Updates to Speakers section

--- src/DTWSpeakers.js
+++ src/DTWSpeakers.js
@@ -3,6 +3,12 @@ import {MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon} from "mdbreact";
 
 class DTWSpeakers extends Component {
   render() {
+
+    const speakerImgStyle = {
+      '-webkit-filter': `grayscale(1)`,
+      filter: `grayscale(1)`
+    };
+    
     return (
         <MDBContainer className="p-5 section dark">
           <MDBRow>
@@ -11,15 +17,21 @@ class DTWSpeakers extends Component {
             </MDBCol>
           </MDBRow>
           <MDBRow>
+            <MDBCol>
+              <h1 className="">&nbsp;</h1>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className={`grey lighten-4 text-black p-1`}>
             <MDBCol size="4">
               <img
                   src="/static/images/speaker.jpg"
                   alt=""
                   className="rounded-circle img mx-auto mt-5"
+                  style={speakerImgStyle}
               />
               <h4 className="font-weight-bold mb-4">You?</h4>
               <p className="dark-grey-text mt-4">
-                <a href="/#" target="_blank"><MDBBtn size="lg" outline color="info">Submit a
+                <a href="/#" target="_blank"><MDBBtn size="lg" color="info">Submit a
                   Proposal <MDBIcon far icon="paper-plane"/></MDBBtn></a>
               </p>
             </MDBCol>
@@ -27,14 +39,13 @@ class DTWSpeakers extends Component {
               <p>In addition to the speakers we've lined up, we're actively looking for speakers for the
                 Detroit Tech
                 Watch for 2019.</p>
-              <p>Detroit Tech Watch welcomes new and seasoned speakers to give a talk in Detroit!</p>
               <h5>What you can expect</h5>
               <ul>
                 <li>Slot times will be roughly 30 minutes</li>
                 <li>For speakers that are not local, we'll provide an allowance for travel and lodging</li>
               </ul>
               <h5>What we're looking for</h5>
-              <a href="schedule" id="schedule">
+
                 <ul>
                   <li>Generally we're looking for talks related to newer technologies that you're using at
                     work to solve
@@ -43,9 +54,9 @@ class DTWSpeakers extends Component {
                   <li>Talks and ideas that inspire the community</li>
                   <li>Talks that you are enthusiastic and excited about!</li>
                 </ul>
-              </a>
             </MDBCol>
           </MDBRow>
+          <a href="schedule" id="schedule"> </a>
         </MDBContainer>
 
     );
