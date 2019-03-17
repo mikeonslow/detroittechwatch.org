3ef6b43a09772f11fef9ddaea7f426f5db5ec88c
mikeonslow <mikeonslow@gmail.com>
2019-03-17 00:52:12 -0400

    Updated front-end to submit email to be added to mailchimp list (still needs front-end validation)

--- src/DTWConnect.js
+++ src/DTWConnect.js
@@ -1,27 +1,55 @@
 import React, {Component} from 'react';
 import {
   MDBContainer, MDBRow, MDBCol,
-  // MDBBtn, MDBInput, MDBFormInline, MDBIcon
+  MDBBtn, MDBInput, MDBFormInline, MDBIcon
 } from "mdbreact";
 
 class DTWConnect extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch('/.netlify/functions/subscribe', {
+      method: 'post',
+      body: JSON.stringify({"subscriber": this.state.value})
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      console.log(data);
+    });
+  }
+
   render() {
+
+    const { value } = this.state;
+
     return (
         <MDBContainer className="p-5">
           <MDBRow>
             <MDBCol size="12">
               <h1>Stay up to Date!</h1>
-              {/*<p className="text-center">Join our mailing list for the latest conference updates. Your email address*/}
-                {/*will be kept private and we'll only update you with with messages relevant to the event.</p>*/}
+              <p className="text-center">Join our mailing list for the latest conference updates. Your email address
+                will be kept private and we'll only update you with with messages relevant to the event.</p>
             </MDBCol>
-            {/*<MDBCol size="12" className="text-center">*/}
-            {/*<MDBFormInline className="md-form mr-auto mb-4 justify-content-center">*/}
-            {/*<MDBInput label="Email Address" hint="youremail@domain.com" type="email" size="lg" className="signup"/>*/}
-            {/*<MDBBtn color="secondary" size="md">sign up <MDBIcon far icon="comments"/></MDBBtn>*/}
-            {/*</MDBFormInline>*/}
-            {/*</MDBCol>*/}
+            <MDBCol size="12" className="text-center">
+              <MDBFormInline className="md-form mr-auto mb-4 justify-content-center">
+                <MDBInput label="Email Address" hint="youremail@domain.com" type="email" size="lg" className="signup" onChange={this.handleChange} value={value}/>
+                <MDBBtn color="secondary" size="md" onClick={this.handleSubmit}>sign up <MDBIcon far icon="comments"/></MDBBtn>
+              </MDBFormInline>
+            </MDBCol>
             <MDBCol size="12">
-              <p className="text-center">Follow us on
+              <p className="text-center">Or, follow us on
                 <a href="https://twitter.com/dettechwatch" target="_blank" rel="noopener noreferrer">Twitter</a>, <a
                     href="https://www.facebook.com/DetroitTechWatch/" target="_blank"
                     rel="noopener noreferrer">Facebook</a> or <a
