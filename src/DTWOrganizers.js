fbbba901487222207e43e45a1e25208ef775793f
mikeonslow <mikeonslow@gmail.com>
2019-06-05 09:41:45 -0400

    Added Daniel to the Organizers section

--- src/DTWOrganizers.js
+++ src/DTWOrganizers.js
@@ -8,7 +8,8 @@ const organizers =
       {"name": "Onorio Catenacci", "imageUrl": "onorio.jpg", "twitterHandle": "OldDutchCap", "githubHandle": ""},
       {"name": "Mike Onslow", "imageUrl": "mike.jpg", "twitterHandle": "mike_onslow", "githubHandle": ""},
       {"name": "Rebecca Farr", "imageUrl": "rebecca.jpg", "twitterHandle": "BB8becca", "githubHandle": ""},
-      {"name": "Kristina Rudolph", "imageUrl": "kristi.jpg", "twitterHandle": "UXUIKris", "githubHandle": ""}
+      {"name": "Kristina Rudolph", "imageUrl": "kristi.jpg", "twitterHandle": "UXUIKris", "githubHandle": ""},
+      {"name": "Daniel Davis", "imageUrl": "danield.jpg", "twitterHandle": "daniel_davis", "githubHandle": ""}
   ];
 
 
