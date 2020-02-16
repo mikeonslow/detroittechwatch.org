40aaecc4172a1b3778c0d26e56bf7dfd33e3ab91
mikeonslow <mikeonslow@gmail.com>
2020-02-15 20:03:46 -0500

    Removed sections that are not ready yet

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React, {Component} from 'react';
-import {MDBContainer, MDBRow, MDBCol} from "mdbreact";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import DTWFixedNavbar from './DTWFixedNavbar'
-import DTWJumbotron from './DTWJumbotron'
-import DTWConnect from './DTWConnect'
-import DTWSpeakers from './DTWSpeakers'
-import DTWFooter from './DTWFooter'
+import React, { Component } from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import DTWFixedNavbar from "./DTWFixedNavbar";
+import DTWJumbotron from "./DTWJumbotron";
+import DTWConnect from "./DTWConnect";
+import DTWSpeakers from "./DTWSpeakers";
+import DTWFooter from "./DTWFooter";
 import DTWCodeofConduct from "./DTWCodeofConduct";
 import DTWOrganizers from "./DTWOrganizers";
 import DTWSponsors from "./DTWSponsors";
 import DTWSchedule from "./DTWSchedule";
 
-import './App.css';
+import "./App.css";
 import DTWGettingThere from "./DTWGettingThere";
 
 class App extends Component {
   render() {
     return (
-        <Router>
-          <MDBContainer fluid className="text-center mt-5">
-            <a href="about" id="about">&nbsp;</a>
-            <MDBRow>
-              <MDBCol>
-                <Route path="/" component={DTWFixedNavbar}/>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol>
-                <DTWJumbotron/>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="section lightest">
-              <MDBCol>
-                <DTWConnect/>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="section lightest">
+      <Router>
+        <MDBContainer fluid className="text-center mt-5">
+          <a href="about" id="about">
+            &nbsp;
+          </a>
+          <MDBRow>
+            <MDBCol>
+              <Route path="/" component={DTWFixedNavbar} />
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol>
+              <DTWJumbotron />
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="section lightest">
+            <MDBCol>
+              <DTWConnect />
+            </MDBCol>
+          </MDBRow>
+          {/* <MDBRow className="section lightest">
               <MDBCol>
                 &nbsp;
               </MDBCol>
@@ -44,8 +46,8 @@ class App extends Component {
               <MDBCol>
                 <DTWSpeakers/>
               </MDBCol>
-            </MDBRow>
-            <MDBRow className="section dark-blue">
+            </MDBRow> */}
+          {/* <MDBRow className="section dark-blue">
               <MDBCol>
                 &nbsp;
               </MDBCol>
@@ -54,39 +56,37 @@ class App extends Component {
               <MDBCol>
                 <DTWSchedule/>
               </MDBCol>
-            </MDBRow>
-            <MDBRow className="section lightest">
+            </MDBRow> */}
+          {/* <MDBRow className="section lightest">
               <MDBCol>
                 <DTWSponsors/>
               </MDBCol>
-            </MDBRow>
-            <MDBRow className="section dark">
+            </MDBRow> */}
+          {/* <MDBRow className="section dark">
               <MDBCol>
                 <DTWGettingThere/>
               </MDBCol>
-            </MDBRow>
-            <MDBRow className="section light">
-              <MDBCol>
-                &nbsp;
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="section light">
-              <MDBCol>
-                <DTWCodeofConduct/>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="section dark">
-              <MDBCol>
-                <DTWOrganizers/>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="section mono">
-              <MDBCol>
-                <DTWFooter/>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-        </Router>
+            </MDBRow> */}
+          <MDBRow className="section light">
+            <MDBCol>&nbsp;</MDBCol>
+          </MDBRow>
+          <MDBRow className="section light">
+            <MDBCol>
+              <DTWCodeofConduct />
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="section dark">
+            <MDBCol>
+              <DTWOrganizers />
+            </MDBCol>
+          </MDBRow>
+          <MDBRow className="section mono">
+            <MDBCol>
+              <DTWFooter />
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </Router>
     );
   }
 }
