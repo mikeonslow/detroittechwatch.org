7cd56ecb1e8408f5f99b5e3380f6f4b16276a6d5
Mike Onslow <mikeonslow@gmail.com>
2020-05-15 00:44:42 -0400

    Update DTWJumbotron.js

--- src/DTWJumbotron.js
+++ src/DTWJumbotron.js
@@ -14,13 +14,13 @@ class DTWJumbotron extends Component {
             <img src="static/images/dtw.png" className="img-fluid" alt="logo" />
           </MDBCol>
           <MDBCol className="col-12 col-lg-4 text-center text-lg-right">
-            <h1 className="h1 brand-warning">Cancelled for 2020</h1>
+            <h1 className="h1 text-warning">Cancelled for 2020</h1>
           </MDBCol>
         </MDBRow>
         <MDBRow>
           <MDBCol>
-            <p className="text-light">
-              With the current state of COVID-19, we've decided to cancel this event. We will still continue to host our monthly meetups virtually, so keep an eye on our meetup page: <a href="https://www.meetup.com/Detroit-Tech-Watch/">https://www.meetup.com/Detroit-Tech-Watch/</a> for updates. 
+            <p className="text-warning">
+              With the current state of COVID-19 and social distancing, we've decided to cancel the event (this year). We will continue to host our monthly meetups virtually, so keep an eye on our meetup page: <a href="https://www.meetup.com/Detroit-Tech-Watch/">https://www.meetup.com/Detroit-Tech-Watch/</a> for updates. 
             </p>
             <p className="text-light">
               A look toward the future of software development. A one day long
